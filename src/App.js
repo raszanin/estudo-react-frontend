ed23d22a0b25d153629297c3f4cf619e46774ab3
Roberto A S Zanin <raszanin@gmail.com>
2022-03-22 13:21:37 -0300

    Compónent

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h3>Hello World</h3>
+    </>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { soma } from './soma'
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(soma(1, 4));
+import App from './App';
+
+//JSX: HTML dentro do Javascript (Javascript XML)
+
+render(<App />, document.getElementById('app'))
